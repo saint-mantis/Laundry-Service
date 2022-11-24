a771c1c453f6534a843e4ab4537e5907f3b78b10
Arun Babu <59362586+saint-mantis@users.noreply.github.com>
2022-11-24 13:20:45 +0530

    initial

---
+++ LaundryService/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+    
+def signup(request):
+    return render(request, 'signup.html')
